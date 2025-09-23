f4395f1dd239ffb8b75269b811d6e3c3536bc6c0
Moamen Khadra <momentadev@gmail.com>
2025-09-23 22:52:45 +0300

    Revert "Fix import statements"
    This reverts commit 9bdb9965ad23e3ef8d31ef13e453773e0e41efe2.

--- ros_task/ros2_ws/src/multi_sensor_validator/multi_sensor_validator/infrared_sensor_node.py
+++ ros_task/ros2_ws/src/multi_sensor_validator/multi_sensor_validator/infrared_sensor_node.py
@@ -1,7 +1,7 @@
 from random import randint
 from std_msgs.msg import Int32
-from sensor_node import SensorNode
-from utility import Utility
+from multi_sensor_validator.sensor_node import SensorNode
+from multi_sensor_validator.utility import Utility
 
 
 class InfraredSensorNode(SensorNode):
--- ros_task/ros2_ws/src/multi_sensor_validator/multi_sensor_validator/logger_node.py
+++ ros_task/ros2_ws/src/multi_sensor_validator/multi_sensor_validator/logger_node.py
@@ -1,25 +1,22 @@
-from utility import Utility
+from multi_sensor_validator.utility import Utility
 from rclpy.node import Node
 from std_msgs.msg import String
 
-
 class LoggerNode(Node):
     """Node that simulates an logger by publishing random range values."""
-
+    
     def __init__(self):
         super().__init__('logger_node')
         self.get_logger().info('Logger Node has been started.')
-        self.subscription = self.create_subscription(
-            String, 'validation_result', self.log_callback, Utility.queue_size)
+        self.subscription = self.create_subscription(String, 'validation_result',self.log_callback, Utility.queue_size)
 
-    def log_callback(self, msg):
+    def log_callback(self,msg):
         # self.get_logger().info(msg.data)
         print(msg.data)
 
 
 def main(args=None):
-    Utility.run_node(args=args, node_factory=lambda: LoggerNode())
-
+    Utility.run_node(args=args,node_factory=lambda:LoggerNode())
 
 if __name__ == '__main__':
     main()
